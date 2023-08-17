326149e63a9a3482b9f78ee348b11dd2435f6272
Sofia B. Manzano <sofiabmanzano@gmail.com>
2023-08-17 12:22:00 +0200

    add colors && header done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,48 @@
+import React, { useEffect, useState } from 'react';
 import logoKuorum from "../../img/kuorum.svg";
+import informationIcon from "../../img/information.svg";
 const Header = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setScrolling(true);
+    } else {
+      setScrolling(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    <header>
-      <figure>
+    <header className=
+    {`fixed top-0 left-0 right-0 flex justify-between p-4 border-b  ${scrolling ? 'py-2' : 'py-4'} transition-all duration-300 ease-in-out z-50`}
+    >
+      <div className="flex items-center">
         <svg
-          width="30"
-          height="30"
+       
+          width="134"
+          height="32"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 134 32"
         >
-          <image href={logoKuorum} alt="Logo Kuorum" />
+          <image href={logoKuorum} alt="Información" />
         </svg>
-       
-      </figure>
+      </div>
+      <div>
+        <svg
+          width="32"
+          height="32"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 32 32"
+        >
+          <image href={informationIcon} alt="Logo Kuorum" />
+        </svg>
+      </div>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#575757",
+        "secondary-color": "#4D7AE3",
+        "light-color": "#E0E0E0" ,
+        "darker-color":"#2C2C2C"
+      },
+    },
   },
   plugins: [],
-}
-
+};
